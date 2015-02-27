314a833d1b7b41efb6a8ede1bc57a5a659a55fa1
donproject <business@thedonproject.com>
2015-02-26 22:06:20 -0800

    basic css styling and test swap

--- crayons.js
+++ crayons.js
@@ -1,8 +1,17 @@
-//(get options? random by default)
-//pack size (show different sections?)
-//shuffle the default crayons
+$(function() {
+  //(get options? random by default)
+  //pack size (show different sections?)
+  //shuffle the default crayons
 
-//sort the shuffled crayons
-//convert hex to hsv
-//sort by h, then s, then v (options?)
-//animate the crayons
+  //sort the shuffled crayons
+  //convert hex to hsv
+  //sort by h, then s, then v (options?)
+  //animate the crayons
+
+  //test swap on button click
+  $('#shuffle').on('click', function() {
+    $('li:eq(1)').
+    after($('li:eq(5)')).
+    insertBefore($('li:eq(5)'));
+  });
+});
