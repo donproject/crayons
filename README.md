a4554c9ed7549a7c2ecbe04c37e0a3532cea81b4
donproject <business@thedonproject.com>
2015-02-26 21:00:54 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# crayons
+
+## An app to shuffle and sort crayons
+
+### References:
+* Crayon colors from [wikipedia](http://en.wikipedia.org/wiki/List_of_Crayola_crayon_colors)
---
+++ crayons.js
@@ -0,0 +1,8 @@
+//(get options? random by default)
+//pack size (show different sections?)
+//shuffle the default crayons
+
+//sort the shuffled crayons
+//convert hex to hsv
+//sort by h, then s, then v (options?)
+//animate the crayons
